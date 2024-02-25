be7c5c22ee12dddea767b93a8c320b1e20004de4
AissEra <66399342+aisser3@users.noreply.github.com>
2024-02-25 17:38:11 +0800

    initial-demo

---
+++ acm-template/src/main/java/com/aisser/acm/MainTemplate.java
@@ -0,0 +1,11 @@
+package com.aisser.acm;
+
+/**
+ * @author AissEr
+ * @created by AissEr on 2024/2/25-18:07
+ */
+public class MainTemplate {
+    public static void main(String[] args) {
+
+    }
+}
