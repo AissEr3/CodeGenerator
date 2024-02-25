ec7326e56ec72fdd4603245d23bf82951b633d1b
AissEr3 <553023755@qq.com>
2024-02-25 21:16:30 +0800

    首次练习使用 Picocli 框架

---
+++ generator-basic/src/main/java/com/aisser/Main.java
@@ -0,0 +1,15 @@
+package com.aisser;
+
+import com.aisser.generator.StaticGenerator;
+
+import java.io.File;
+
+/**
+ * @author AissEr
+ * @created by AissEr on 2024/2/25-19:42
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
