9a82844545a41138d111d69b0348ea599ea3372d
AissEr3 <553023755@qq.com>
2024-02-26 16:49:32 +0800

    jar包配置

--- generator-basic/src/main/java/com/aisser/Main.java
+++ generator-basic/src/main/java/com/aisser/Main.java
@@ -11,9 +11,6 @@ import java.io.File;
  */
 public class Main {
     public static void main(String[] args) {
-//        args = "generate -l true -a aisser -s".split(" ");
-//        args = "list".split(" ");
-        args = "config".split(" ");
         CommandExecutor commandExecutor = new CommandExecutor();
         commandExecutor.doExecute(args);
     }
