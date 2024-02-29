61bf7e8ceb1a36846c4bb9e44765a1e4e9aec23e
AissEr3 <553023755@qq.com>
2024-02-29 22:07:56 +0800

    添加Spring Boot项目模板

---
+++ generator-demo-project/springboot-init/src/main/java/com/aisser/springbootinit/mapper/PostMapper.java
@@ -0,0 +1,15 @@
+package com.aisser.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.aisser.springbootinit.model.entity.Post;
+
+/**
+ * 帖子数据库操作
+ */
+public interface PostMapper extends BaseMapper<Post> {
+
+}
+
+
+
+
